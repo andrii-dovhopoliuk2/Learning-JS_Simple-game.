5568aa72cb42379fca6022bbca14beb44a3afff4
Andrii-Dovhopoliuk2 <57150972+Andrii-Dovhopoliuk2@users.noreply.github.com>
2020-06-08 18:15:16 +0300

    release final

--- index.js
+++ index.js
@@ -12,14 +12,60 @@ function randomColor(){
 
 var $start = document.querySelector("#start")
 var $game = document.querySelector("#game")
+var $time = document.querySelector("#time") 
+var $timeHeader = document.querySelector("#time-header") 
+var $result = document.querySelector("#result")
+var $resultHeader = document.querySelector("#result-header") 
+var $gameTime = document.querySelector('#game-time')
 var score = 0 
+function show($element){
+    $element.classList.remove('hide')
+}
+function hide($element){
+    $element.classList.add('hide')
+}
+
+var isGameStarted = false
 $start.addEventListener('click',start)
 $game.addEventListener('click',boxGameClick)
+$gameTime.addEventListener('input',setGameTime)
+
 function start(){
+    setGameTime()
+    score = 0
+    $gameTime.setAttribute('disabled',"true")
+    hide($resultHeader)
+    show($timeHeader)
+    isGameStarted = true
     $game.style.background = "white"
-    $start.classList.add('hide')
+    hide($start)
     //$start.className = 'hide'
+   
+    var interval = setInterval(function(){
+        var time = parseFloat($time.textContent)
+        if(time <= 0){
+            clearInterval(interval)
+            endGame()
+        }else{
+            $time.textContent = (time - 0.1).toFixed(1)
+        }
+    },100)
     renderBox()
+}
+function setGameTime(){
+    hide($resultHeader)
+    show($timeHeader)
+    $time.textContent = +$gameTime.value // "+$gameTime.value" = parseInt.$gameTime.value  
+}
+function endGame(){
+    isGameStarted = false
+    show($start)
+    $game.style.background = "#ccc"
+    $game.innerHTML = ""
+    hide($timeHeader)
+    $result.textContent = score.toString()
+    show($resultHeader)
+    $gameTime.removeAttribute('disabled')
 }
 function renderBox(){
     $game.innerHTML = ""
@@ -35,6 +81,9 @@ function renderBox(){
     box.setAttribute("data-box",true)
 }    
 function boxGameClick(event){
+    if(!isGameStarted){
+        return
+    }
     if(event.target.dataset.box){
         score++
         renderBox()
